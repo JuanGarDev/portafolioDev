9d744fdea91ab862f32817f045f28a473a16bb22
Juan GarciaPC <fnxbeats@outlook.com>
2023-03-27 00:03:33 -0600

    Agregamos cambios

--- funciones.js
+++ funciones.js
@@ -1,12 +1,9 @@
 const toggle = document.getElementById('modo');
 const body = document.querySelector('body');
-const tarjeta = document.querySelector('#backend');
-const tarjeta2 = document.querySelector('#frontend');
 
 toggle.addEventListener('click', function(){
     this.classList.toggle('fa-moon');
     if(this.classList.toggle('fa-sun')){
-        
         body.style.background = 'linear-gradient(0deg, rgba(250,214,164,1) 0%, rgba(0,200,245,1) 11%, rgba(58,71,213,1) 100%)';
         body.style.transition = 'background 1s linear';
     }else{
