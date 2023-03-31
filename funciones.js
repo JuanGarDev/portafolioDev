3a5a424891164746761044308bebaf69260e210b
Juan GarciaPC <fnxbeats@outlook.com>
2023-03-30 23:24:43 -0600

    Agregamos enlaces

--- funciones.js
+++ funciones.js
@@ -1,13 +1,1 @@
-const toggle = document.getElementById('modo');
-const body = document.querySelector('body');
 
-toggle.addEventListener('click', function(){
-    this.classList.toggle('fa-moon');
-    if(this.classList.toggle('fa-sun')){
-        body.style.background = 'linear-gradient(0deg, rgba(250,214,164,1) 0%, rgba(0,200,245,1) 11%, rgba(58,71,213,1) 100%)';
-        body.style.transition = 'background 1s linear';
-    }else{
-        body.style.background = 'linear-gradient(0deg, rgba(71,20,49,1) 0%, rgba(20,24,71,1) 11%, rgba(7,9,25,1) 100%)';
-        body.style.transition = 'background 1s linear';
-    }
-});
